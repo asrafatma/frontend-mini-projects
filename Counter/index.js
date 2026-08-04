f3c47e1afe550acd2cde37df13a923acbd702241
asrafatma <waziralamasra@gmail.com>
2026-08-04 21:57:18 +0500

    Initialize counter element references in index.js

---
+++ Counter/index.js
@@ -0,0 +1,4 @@
+const increase = document.getElementById("increase")
+const reset = document.getElementById("reset")
+const decrease = document.getElementById("decrease")
+const counter = document.getElementById("counter")
