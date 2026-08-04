d41dcb9783c3a6ebba182184d964ccda85b468d5
asrafatma <waziralamasra@gmail.com>
2026-08-04 21:58:22 +0500

    Implement decrease functionality for counter in index.js

--- Counter/index.js
+++ Counter/index.js
@@ -2,3 +2,10 @@ const increase = document.getElementById("increase")
 const reset = document.getElementById("reset")
 const decrease = document.getElementById("decrease")
 const counter = document.getElementById("counter")
+
+let count = 0;
+
+decrease.onclick - function(){
+    count--;
+    counter.textContent = count;
+}
