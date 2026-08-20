f4709496cdb6844f95aef3efb840edf237902cf1
asrafatma <waziralamasra@gmail.com>
2026-08-20 21:44:04 +0500

    Implement number guessing logic and update result display

---
+++ NumberGuessingGame/index.js
@@ -0,0 +1,18 @@
+let input = document.getElementById("input");
+let button = document.getElementById("button");
+const num = Math.floor(Math.random()*100)+1;
+let result = document.getElementById("result");
+
+let randomNumber;
+
+button.onclick = function(){
+    if(randomNumber == num){
+        result.textContent("You guessed the number correctly!!");
+    } else if (randomNumber > num) {
+        result.textContent("Too High!!");
+    } else if (randomNumber < num) {
+        result.textContent("Too Low!1");
+    } else {
+        result.textContent("Invalid Number!!");
+    }
+}
