9c736666e94f6058dd04fcb0bd177ae820500316
asrafatma <waziralamasra@gmail.com>
2026-08-20 21:49:32 +0500

    Initialize game state variables for attempts and running status

--- NumberGuessingGame/index.js
+++ NumberGuessingGame/index.js
@@ -4,6 +4,9 @@ const min = 1;
 const max = 100;
 const answer = Math.floor(Math.random() * (max - min + 1)) + min;
 let result = document.getElementById("result");
+const maxAttempts = 3;
+let attempts = 0;
+let running = true;
 
 let randomNumber;
 
