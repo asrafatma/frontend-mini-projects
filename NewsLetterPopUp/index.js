abb073d7f2f4c75833b0ac1f0f608ec58d527ee7
asrafatma <waziralamasra@gmail.com>
2026-08-24 21:49:49 +0500

    Add hidePopup function to manage popup visibility

--- NewsLetterPopUp/index.js
+++ NewsLetterPopUp/index.js
@@ -2,3 +2,8 @@ function showPopup() {
     const popup = document.getElementById('popup');
     popup.classList.add('active');
 }
+
+function hidePopup() {
+    const popup = document.getElementById('popup');
+    popup.classList.remove('active');
+}
